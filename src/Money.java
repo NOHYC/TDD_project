230352af75b1a001c095def6f379263732f21151
NOH <nyc1517@gmail.com>
2022-02-12 17:42:30 +0900

    chapter 6,7,8 : Money 상위클래스 구현, 추상클래스 구현, 팩토리 메서드 - 공통된 코드를 상위 클래스(Money)와 하위 클래스(Dollar)로 분류 - 메소드 일치, 리팩토링 - 팩토리 메서드 도입

--- src/Money.java
+++ src/Money.java
@@ -1,34 +1,39 @@
-abstract class Money{
+abstract class Money {
     protected int amount;
+
     abstract Money times(int multiplicaion);
-    static Money dollar(int amount){
+
+    static Money dollar(int amount) {
         return new Dollar(amount);
     }
-    static Money franc(int amount){
+
+    static Money franc(int amount) {
         return new Franc(amount);
     }
-    public boolean equals(Object object){
+
+    public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
 }
 
 
-class Dollar extends Money{
-    Dollar(int amount){
+class Dollar extends Money {
+    Dollar(int amount) {
         this.amount = amount;
     }
-    Money times(int multiplicaion){
+
+    Money times(int multiplicaion) {
         return new Dollar(amount * multiplicaion);
     }
 }
 
-class Franc extends Money{
-    Franc(int amount){
+class Franc extends Money {
+    Franc(int amount) {
         this.amount = amount;
     }
-    Money times(int multiplicaion){
+
+    Money times(int multiplicaion) {
         return new Franc(amount * multiplicaion);
     }
-
 }
