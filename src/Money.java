9951b85645c2d3a4e4c09485c7fad56625d91bfe
NOH <nyc1517@gmail.com>
2022-02-19 15:34:43 +0900

    chapter 11 : 하위 클래스 생성자 제거 1. 하위 클래스에 생성자 밖에 없음 2. 하위 클래스 참조를 상위 클래스 참조로 변경 3. 동치성 테스트 코드가 충분한 지 확인 4. 하위 클래스 참조 테스트 코드 제거 5. 과도한 테스트 코드 제거 6. 테스트 성공

--- src/Money.java
+++ src/Money.java
@@ -7,10 +7,10 @@ class Money {
         this.currency = currency;
     }
     static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     public boolean equals(Object object) {
@@ -25,15 +25,5 @@ class Money {
     }
     Money times(int multiplication) {
         return new Money(amount * multiplication, currency);
-    }
-}
-class Dollar extends Money {
-    Dollar(int amount, String currency) {
-        super(amount, currency);
-    }
-}
-class Franc extends Money {
-    Franc(int amount, String currency) {
-        super(amount, currency);
     }
 }
