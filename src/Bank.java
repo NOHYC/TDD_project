4a6b59e1cc1ba1582c326cd1e4ba619b6ee66dfc
NOH <nyc1517@gmail.com>
2022-02-20 13:00:37 +0900

    chapter 14 : 환율 적용(인터페이스 수정) 1. Franc 과 Dollar의 환율을 비교하는 테스트 코드 작성 2. 프랑을 달러로 변환은 Bank에서 처리 예정 3. reduce 메서드에서 처리하기 위해서 매개 변수 개수 변경 (Bank,String) 1개 -> 2개 4. reduce가 포함된 인터페이스, 클래스 모두 변경(공용이기 때문)

---
+++ src/Bank.java
@@ -0,0 +1,9 @@
+class Bank {
+    Money reduce(Expression source, String to) {
+        return source.reduce(this, to);
+    }
+
+    int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+    }
+}
